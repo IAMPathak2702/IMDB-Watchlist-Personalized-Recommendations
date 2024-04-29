b2ba970b04a555aabcd0e6281fa70cedd747e268
Ved Praksh <vp.ved.vpp@gmail.com>
2024-04-30 01:10:44 +0530

    added stremlit.py for Pouplar Books recommendation system

---
+++ Templates/Popularity_based_recommendation.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Book Recommendation",
+    page_icon="📚",
+    layout="wide"
+)
+
+st.title(" Popular Books based on User Ratings")
+
+df = pd.read_csv("src/data/popular_df.csv")
+
+def display_star_rating(rating):
+    full_star = '⭐️'
+    empty_star = '⭐'
+    
+    stars = int(rating)
+    
+    stars_display = f"{full_star * stars} ({stars})"
+    
+    return stars_display
+
+select = st.selectbox(label="Select a book", options=df['Book-Title'], index=0)
+
+if select:
+    q = df.index[df['Book-Title'] == select][0]
+    st.image(df.loc[q, 'Image-URL-S'])
+
+# Display the images in a grid layout
+num_rows = 5
+num_cols = 10
+
+df_subset = df.head(num_rows * num_cols)
+
+for i in range(num_rows):
+    cols = st.columns(num_cols)
+    for j in range(num_cols):
+        index = i * num_cols + j
+        if index < len(df_subset):
+            rating = df_subset.iloc[index]['avg_rating']  # Use 'avg_rating' instead of 'avg-rating'
+            caption = f"{df_subset.iloc[index]['Book-Title']}\n{display_star_rating(rating)}"
+            cols[j].image(df_subset.iloc[index]['Image-URL-S'], caption=caption, use_column_width=True)
